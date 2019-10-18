07167832974d74e7792ed04d0c80f79f64e6443f
Lazarus9000 <jegharen@gmail.com>
2019-10-18 22:26:57 +0100

    playback testing

--- pytest.py
+++ pytest.py
@@ -1,8 +1,23 @@
 import pygame
-file = 'some.mp3'
+file = "some.mp3"
 pygame.init()
 pygame.mixer.init()
+
+#Sound doesnt support mp3
+#music = pygame.mixer.Sound(file)
+#pygame.mixer.Sound.play(music,0,0,30000)
+
 pygame.mixer.music.load(file)
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy(): 
-  pygame.time.Clock().tick(10)
+pygame.mixer.music.set_volume(0.0)
+channel = pygame.mixer.music.play()
+
+#while pygame.mixer.Sound.get_busy(): 
+i = 0
+while i < 10000:
+ volCalc = min(1.0, i/10000)
+ pygame.mixer.music.set_volume(volCalc)
+ #pygame.time.Clock().tick(10)
+ print(volCalc)
+ i += 1
+else:
+ print("tock")
