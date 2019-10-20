df3a3769d6bb0670ff6637eb561f38e3fb02ce5e
Lars Knudsen <jegharen@gmail.com>
2019-10-20 21:27:00 +0200

    Update rfidaudiotest.py

--- rfidaudiotest.py
+++ rfidaudiotest.py
@@ -21,10 +21,13 @@ pygame.mixer.init()
 pygame.mixer.music.load(file)
 pygame.mixer.music.set_volume(1.0)
 
+cardread = ''
+
 try:
         id, text = reader.read()
         print(id)
         print(text)
+        cardread = text
 finally:
         GPIO.cleanup()
         channel = pygame.mixer.music.play()
@@ -32,13 +35,13 @@ finally:
 #while pygame.mixer.Sound.get_busy(): 
 i = 0
 while i < 10000:
- if text == "1" 
+ if cardread == '1' 
   volCalc = min(1.0, i/10000)
   pygame.mixer.music.set_volume(volCalc)
   #pygame.time.Clock().tick(10)
   print(volCalc)
   i += 1
- if text == "2"
+ if cardread == '2'
   print(syke)
 else:
  print("tock")     
