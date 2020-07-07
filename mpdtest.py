fca89010823ea7bb71ab816a41356f50abd5749c
Kaptajn Knudsen <jegharen@gmail.com>
2020-07-07 18:48:37 +0100

    Added a bit of functionality to the test script

--- mpdtest.py
+++ mpdtest.py
@@ -4,13 +4,15 @@ import time
 client = MPDClient()               # create client object
 client.timeout = 10                # network timeout in seconds (floats allowed), default: None
 client.idletimeout = None          # timeout for fetching the result of the idle command is handled seperately, default: None
-client.connect("localhost", 6600)  # connect to localhost:6600
+client.connect("192.168.1.242", 6600)  # connect to localhost:6600
 print(client.mpd_version)          # print the MPD version
 print(client.find("any", "spillebillen")) # print result of the command "find any house"
-client.add("spotify:track:0VR0uoOiCMRtPVwh49dKrq")
+client.clear()
+client.setvol(50)
+client.add("spotify:track:4QWRvlXU8dRUobH9YCDrIY")
 print(client.playlist())
 client.play()
-time.sleep(3)
+time.sleep(15)
 client.stop()
 client.close()                     # send the close command
 client.disconnect()
