6b35593ebe5926f3f025dafa6adf540100ed13e3
Kaptajn Knudsen <jegharen@gmail.com>
2020-07-17 21:24:42 +0100

    Fixed music playback POC

--- RFIDmpdtest.py
+++ RFIDmpdtest.py
@@ -9,20 +9,22 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 thisdict = {
-  "PawPatrol": "7n70tzvZDfdAL1j9XqsAbH",
+  "PawPatrol": "2efkanyOeo0Vzkli3HfcuU",
   "JaJaDingDong": "ppcOKYrNRnGQbWc5qI5A1w"
 }
 
 def playsong(id):
   client.stop()
   client.clear()
-  print(id)
-  print(thisdict)
-  trackid = thisdict[id]
-  track = "spotify:track:" + trackid
-  print(track)
-  client.add(track)
-  client.play()
+  #print(id)
+  #print(thisdict)
+  #if id does not exist, then music playback stops
+  if id in thisdict:
+    trackid = thisdict[id]
+    track = "spotify:track:" + trackid
+    print(track)
+    client.add(track)
+    client.play()
 
 client = MPDClient()               # create client object
 client.timeout = 10                # network timeout in seconds (floats allowed), default: None
@@ -31,17 +33,21 @@ client.connect("192.168.1.242", 6600)  # connect to localhost:6600
 print(client.mpd_version)          # print the MPD version
 print(client.find("any", "spillebillen")) # print result of the command "find any house"
 client.clear()
-client.setvol(50)
+client.setvol(20)
 
-try:
-        id, text = reader.read()
-        print(id)
-        print(text)
-        playsong(text.strip())
-finally:
-        GPIO.cleanup()
-        client.close()                     # send the close command
-        client.disconnect()
+playing = True
+
+while playing == True:
+	time.sleep(0.1)
+	try:
+        	id, text = reader.read()
+        	#print(id)
+        	print(text)
+        	playsong(text.strip())
+	finally:
+        	GPIO.cleanup()
+#        	client.close()                     # send the close command
+#        	client.disconnect()
 
         
 
